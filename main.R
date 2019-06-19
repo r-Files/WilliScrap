c286c9351a23f88bbfe4fe8bcf9ea081ad06259a
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2019-06-19 23:20:47 +0200

    saving
    saving csv
    
    works fine; warning messages ?!?!?!?
    lastSeen not working

--- main.R
+++ main.R
@@ -5,15 +5,29 @@ pacman::p_load(data.table, rvest, dplyr, stringr, jsonlite, httr)
 source("get_links.R", encoding='UTF-8')
 source("single_scrap.R", encoding='UTF-8')
 
-result <- data.table()
+
+# Configuration
+scrapfile <- 'flats.csv'
 test_link <- "https://www.willhaben.at/iad/immobilien/eigentumswohnung/wien/wien-1010-innere-stadt/"
 
+
+# read existing scraps into results
+if(file.exists(scrapfile))
+{
+  result <- fread(scrapfile, colClasses = 'character', header=TRUE, key='id', encoding='UTF-8')
+  setkey(result,'id')
+} else {
+  result <- data.table()
+}
+
+# fetch all links
 all_links <- get_links(test_link)
 
-for(number in 1:20){#nrow(all_links)){
+# fetch ads to all links
+for(number in 1:10){#nrow(all_links)){
   # Fetch one ad and measure the time
   t0 <- Sys.time()
-  cat(number, all_links$links[[number]],'\n')
+  cat(number,'/',nrow(all_links),all_links$links[[number]],'\n')
   one_ad <- single_scrap(all_links$links[[number]])
   t1 <- Sys.time()
 
@@ -28,12 +42,13 @@ for(number in 1:20){#nrow(all_links)){
   }
   else{
     print('Not new')
-    result[one_ad[["id"]], 'lastSeen'] <- Sys.Date()
+    result[one_ad[["id"]]][['lastSeen']] <- Sys.Date()
   }
 
   # sleep 1-10 times longer than response_delay
   response_delay <- as.numeric(t1-t0)
-  Sys.sleep(runif(1, min = 1, max = 10)*response_delay)
+  Sys.sleep(runif(1, min = 1, max = 10)*as.numeric(t1-t0))
 }
-one_ad <- single_scrap(all_links$links[[1]])
 
+# Save scrap to csv
+write.csv(result, scrapfile, quote=TRUE, row.names=FALSE, fileEncoding ='UTF-8')
