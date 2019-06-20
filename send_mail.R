c1f13c33d1f70d4db25135950b4e6e5f19a00c9b
r-Files <33875788+r-Files@users.noreply.github.com>
2019-06-20 21:17:17 +0200

    Update send_mail.R
    more recipients are now possible

--- send_mail.R
+++ send_mail.R
@@ -1,3 +1,6 @@
+# library(jsonlite)
+# library(magrittr)
+
 send_mail <- function(to, Body, Subject = "Automatic Scrapper Report"){
   
   # read the credentials for the mail account
@@ -7,20 +10,25 @@ send_mail <- function(to, Body, Subject = "Automatic Scrapper Report"){
   # single quotes 
   mailFrom <- paste0("'", config$gmail$account, "'")
   password <- paste0("'", config$gmail$password, "'")
-  mailTo   <- paste0("'", to, "'")
   Subject  <- paste0("'", Subject, "'")
   Body     <- paste0("'", Body, "'")
   
-
-  # Create a new SMTP-client and send the mail with Powershell.
-  system2("powershell", args = c(c("$EmailFrom =", mailFrom) %>% paste(collapse = ""),
-                                 c("$EmailTo ="  , mailTo)   %>% paste(collapse = ""),
-                                 c("$Subject ="  , Subject)  %>% paste(collapse = ""),
-                                 c("$Body ="     , Body)     %>% paste(collapse = ""),
-                                 "$SMTPClient = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587)",
-                                 "$SMTPClient.EnableSsl = $true",
-                                 c("$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(", mailFrom, ",", password, ")") %>% paste(collapse = ""),
-                                 "$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)") %>% paste(collapse = ";"))
+  lapply(to, function(to_single){
+    mailTo   <- paste0("'", to_single, "'")
+    
+    # Create a new SMTP-client and send the mail with Powershell.
+    system2("powershell", args = c(c("$EmailFrom =", mailFrom) %>% paste(collapse = ""),
+                                   c("$EmailTo ="  , mailTo)   %>% paste(collapse = ""),
+                                   c("$Subject ="  , Subject)  %>% paste(collapse = ""),
+                                   c("$Body ="     , Body)     %>% paste(collapse = ""),
+                                   "$SMTPClient = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587)",
+                                   "$SMTPClient.EnableSsl = $true",
+                                   c("$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(", mailFrom, ",", password, ")") %>% paste(collapse = ""),
+                                   "$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)") %>% paste(collapse = ";"))
+  })
 }
 
-# send_mail(to = "fabian.pribahsnik@gmx.at", Body = "Test")
+# send_mail(to = c("aaaaa@gmx.at", "bbbb@gmx.at"), Body = "Test")
+
+
+
