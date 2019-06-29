c5cedd1c4c4591f62427456e740fc0e36b04c5fd
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2019-06-30 00:18:16 +0200

    Update main.R

--- main.R
+++ main.R
@@ -47,15 +47,15 @@ if (file.exists(configuration$config$scrapfile))
 # get all links
 new_ads <- lapply(seq_along(configuration$willhaben$pages), function(i) {
   links_per_district <- get_links(configuration$willhaben$pages[i])
-  
+
   temp_data <- data.table()
-  
+
   # fetch ads to all links
   for (number in 1:nrow(links_per_district)) {
-    
+
     # find unique ad-number in already scraped file and return row-number
     row_index <- match(links_per_district$ad_id[[number]], result$id)
-    
+
     if (!is.na(row_index))
     {
       result[row_index, lastSeen := format(Sys.Date(), format = "%Y-%m-%d")]
@@ -65,17 +65,17 @@ new_ads <- lapply(seq_along(configuration$willhaben$pages), function(i) {
       t0 <- Sys.time()
       one_ad <- single_scrap(links_per_district$links[[number]])
       t1 <- Sys.time()
-      
+
       one_ad$firstSeen <- format(Sys.Date(), format = "%Y-%m-%d")
       one_ad$lastSeen <- format(Sys.Date(), format = "%Y-%m-%d")
-      
+
       message <- "new ad"
-      
+
       temp_data <- rbind(one_ad, temp_data, fill = TRUE)
-      
+
       # sleep 1-3 times longer than response_delay
       response_delay <- as.numeric(t1 - t0)
-      Sys.sleep(runif(1, min = 1, max = 10) * as.numeric(t1 - t0))
+      Sys.sleep(runif(1, min = 1, max = 3) * as.numeric(t1 - t0))
     }
     cat(
       'District: ',
