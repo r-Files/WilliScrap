418ce22227e2e362ca8f699ebe648a6d5b94b0b1
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2019-06-29 14:01:45 +0200

    merge fix

--- single_scrap.R
+++ single_scrap.R
@@ -2,12 +2,8 @@ single_scrap <- function(link){
 
   single_flat <- read_html(link, encoding = "latin-1")
 
-single_scrap <- function(link){
-  
-  single_flat <- read_html(link, encoding = "latin-1")
-  
   log <- data.table()
-  
+
   # get the price (in raw format)
   log$price <-
     single_flat %>%
@@ -15,10 +11,10 @@ single_scrap <- function(link){
     html_nodes("title") %>%
     html_text() %>%
     str_extract(pattern = "(\u20AC) \\d{1,3}(\\.\\d{3})*") %>%  # find "Euro 4.995.000"
-    str_extract("\\d{1,3}(\\.\\d{3})*") %>% # find only the number 
+    str_extract("\\d{1,3}(\\.\\d{3})*") %>% # find only the number
     str_replace_all("\\.", "") %>% # delete the thousand separator
     as.numeric()
-  
+
   # get willhaben-Code:
   log$id <-
     single_flat %>%
@@ -27,7 +23,7 @@ single_scrap <- function(link){
     unique() %>%
     str_extract(pattern = "\\d+") %>%
     as.integer()
-  
+
   # get last modified:
   log$last_modified <-
     single_flat %>%
@@ -35,7 +31,7 @@ single_scrap <- function(link){
     html_text() %>%
     unique() %>%
     str_extract(pattern = "(\\d+)\\.(\\d+)\\.(\\d+) (\\d+):(\\d+)")
-  
+
   # get ad-title
   log$ad_title <-
     single_flat %>%
@@ -65,8 +61,8 @@ single_scrap <- function(link){
   #  +) Zusatzinformationen
   #  +) Preis - Detailinformation
   all_boxes <- single_flat %>% html_nodes("[class='box-block ']") # class with exactly this name!
-  
-  
+
+
   # Objektinformation and Ausstattung und FreiflÃ¤che are (always?!) double-columned
   # The bold text is extracted with:
   bold_text <-
@@ -82,10 +78,10 @@ single_scrap <- function(link){
     html_text() %>%
     str_replace_all("\\r|\\n", "") %>%
     str_trim(side = "both")
-  
+
   # add the information extracted previously to the log
   log[, (bold_text) := as.list(simple_text)]
-  
+
   # In some rare cases the ad doesn't have a living area provided so we first
   # check if this information is available and then extract the living area as
   # integer without dimensions
@@ -93,12 +89,12 @@ single_scrap <- function(link){
     log[, Wohnfläche := Wohnfläche %>% str_extract("\\d+") %>% as.integer()]
   else
     log[, Wohnfläche := NA_integer_]
-  
+
   # calculate the price per square meter
   log[, price_per_square_meter := price / Wohnfläche]
-  
+
   ##SDCOLS!!
-  # for (col in conv_info) 
+  # for (col in conv_info)
   #   set(results, j = col, value = paste0("\"", results[[col]], "\""))
 }
 
